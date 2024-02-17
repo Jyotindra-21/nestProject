4559c15b3ce6d5771f789cc09c83cf33a62e8d06
jyotindra kushwah <jyotindra2421@gmail.com>
2024-02-17 11:29:12 +0530

    nest Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Carousel from "./components/Carousel/Carousel";
+import MainSlide from "./components/MainSec/MainSlide";
+import MainSec from "./components/MainSec/MainSec";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Carousel />
+      <MainSec />
+    </>
+  );
+}
+
+export default App;
