2678185426033c04b58e64b9a51470aabf9c5d2c
jyotindra kushwah <jyotindra2421@gmail.com>
2024-04-07 12:07:31 +0530

    deploy 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Carousel from "./components/Carousel/Carousel";
-import MainSlide from "./components/MainSec/MainSlide";
 import MainSec from "./components/MainSec/MainSec";
 
 function App() {
